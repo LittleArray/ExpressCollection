d44d8a5c50919fe3eac51afeff6c4ae1c773b26b
FFSHAOZI <ffshaozi@outlook.com>
2023-07-29 19:48:27 +0800

    添加图片转base64功能

--- app/src/main/java/top/ffshaozi/expresscollection/ui/screen/intent/SubmitIntent.kt
+++ app/src/main/java/top/ffshaozi/expresscollection/ui/screen/intent/SubmitIntent.kt
@@ -1,13 +1,17 @@
 package top.ffshaozi.expresscollection.ui.screen.intent
 
-import android.content.ContentResolver
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import top.ffshaozi.expresscollection.config.Setting.USER_NAME
 import top.ffshaozi.expresscollection.ui.screen.state.AppState.cr
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 
 class SubmitViewModel : ViewModel() {
@@ -16,16 +20,32 @@ class SubmitViewModel : ViewModel() {
     private var _userName= MutableStateFlow(String());
     val userName = _userName
     private var bitmap: Bitmap? = null
+    private var base64: String? = null
     init {
         _userName.value = USER_NAME
     }
     fun sendContextIntent(str:String){
         _contentText.value = str
     }
+    @OptIn(DelicateCoroutinesApi::class)
     fun sendImgIntent(uri: Uri){
         bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri))
+        GlobalScope.launch {
+            base64 = byte2Base64(bitmap2Byte(bitmap))
+        }
         _contentText.value = "选择图片路径:\n$uri\n图片宽:${bitmap!!.width}\n图片高:${bitmap!!.height}"
 
+    }
+    private fun bitmap2Byte(bitmap: Bitmap?): ByteArray? {
+        if (null == bitmap) throw NullPointerException()
+        // if (null == bitmap) return null;
+        val outputStream = ByteArrayOutputStream()
+        //把bitmap100%高质量压缩 到 output对象里
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
+    }
+    private fun byte2Base64(imageByte: ByteArray?): String? {
+        return if (null == imageByte) null else Base64.getEncoder().encodeToString(imageByte)
     }
     fun submit(){
 
